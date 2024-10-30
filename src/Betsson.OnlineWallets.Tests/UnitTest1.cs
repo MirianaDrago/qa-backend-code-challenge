8ba2dde78ef330b0a4ffd6cb09ebd7cdabbadcee
Miriana Drago <miriana.drago@gmail.com>
2024-10-30 19:57:38 +0100

    addition of tests project

---
+++ src/Betsson.OnlineWallets.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Betsson.OnlineWallets.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
